f622fc0268d73c1cfc11ef3c9c80c0a071efe787
gege <googege@icloud.com>
2018-10-26 20:24:36 +0800

    Create 惰性生成器.go

---
+++ 惰性生成器.go
@@ -0,0 +1,34 @@
+package main
+
+import "fmt"
+
+var resume chan int
+
+func integers() chan int {
+	yield := make(chan int)
+	count := 0
+	go func() {
+		for {
+			yield <- count
+			count++
+		}
+	}()
+	return yield
+}
+
+func generateInteger() int {
+	return <-resume
+}
+
+func main() {
+	resume = integers()
+	fmt.Println(generateInteger())
+	fmt.Println(generateInteger())
+	fmt.Println(generateInteger())
+	fmt.Println(generateInteger())
+	fmt.Println(generateInteger())
+	fmt.Println(generateInteger())
+	fmt.Println(generateInteger())
+	fmt.Println(generateInteger())
+
+}
