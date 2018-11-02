c7adec24eee8e7bd5a95930ffdc265f2b17a9c5b
gege <googege@icloud.com>
2018-11-02 21:09:41 +0800

    Update 几种ok模式.go

--- 几种ok模式.go
+++ 几种ok模式.go
@@ -6,6 +6,7 @@ import "fmt"
 var a = make(map[string]string)
 
 func main() {
+	
 	//1 测试 map中的是否存在某个键
 	var a1 AT
 	var Jet interface{}
@@ -14,17 +15,20 @@ func main() {
 		println(v)
 	}
 	
+	
 	// 2 某对象是否实现了接口
 	
 	if v,ok := a1.(b);ok {
 		println(v)
 	}
 	
+	
 	//3 空接口的运用
 	
 	if v,ok := Jet.(int);ok {
 		fmt.Println(v)
 	}
+	
 	
 	//4 空接口的switch运用
 
@@ -37,12 +41,29 @@ func main() {
 		fmt.Println("err")
 	}
 	
+	
 	//5 recover 中的运用。
 	
 	if ok := recover();ok {
 		fmt.Println(ok)
 	}
 	
+	
+	//6 检测一个通道ch是否关闭 range 方法检测
+	
+    	for input := range ch {
+		...
+    	}
+	
+	
+	//7 检测一个通道ch是否关闭，ok方法。 
+	
+    	for {
+        if input, open := <-ch; !open {
+            break // 通道是关闭的
+	    }
+    	}
+		
 }
 
 type AT interface {
