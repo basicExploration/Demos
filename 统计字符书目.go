cb1c5bf5c52bfe54703785bb36397b85740a1951
gege <googege@icloud.com>
2018-11-14 20:33:26 +0800

    Create 统计字符书目.go

---
+++ 统计字符书目.go
@@ -0,0 +1,56 @@
+//统计输入的字符书目，也可以使用正则将空格去除掉后再次统计书目
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+	"os"
+	"sort"
+	"unicode"
+	"unicode/utf8"
+)
+
+func main() {
+	middleSlice := make([]int, 0)
+	counts := make(map[rune]int)    // counts of Unicode characters
+	var utflen [utf8.UTFMax + 1]int // count of lengths of UTF-8 encodings
+	invalid := 0                    // count of invalid UTF-8 characters
+	file, err := os.Open("./t.svg")
+	if err != nil {
+		fmt.Print("err :\n", err)
+	}
+	in := bufio.NewReader(file)
+	for {
+		r, n, err := in.ReadRune() // returns rune, nbytes, error
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "charcount: %v\n", err)
+			os.Exit(1)
+		}
+		if r == unicode.ReplacementChar && n == 1 {
+			invalid++
+			continue
+		}
+		counts[r]++
+		utflen[n]++
+	}
+	fmt.Printf("rune\tcount\n")
+	for c, n := range counts {
+		middleSlice = append(middleSlice, n)
+		fmt.Printf("%q\t%d\n", c, n)
+	}
+	fmt.Print("\nlen\tcount\n")
+	for i, n := range utflen {
+		if i > 0 {
+			fmt.Printf("%d\t%d\n", i, n)
+		}
+	}
+	if invalid > 0 {
+		fmt.Printf("\n%d invalid UTF-8 characters\n", invalid)
+	}
+	sort.Ints(middleSlice)
+	fmt.Println("使用最多的字符的它自己的数量是：", middleSlice[len(middleSlice)-1])
+}
