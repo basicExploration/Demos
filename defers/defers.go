5d09b4efac6c7e3a5ad99e3afaa24e4de807b830
gege <googege@icloud.com>
2018-11-20 11:45:15 +0800

    Update defers.go

--- defers/defers.go
+++ defers/defers.go
@@ -1,8 +1,31 @@
-package defers
+package main
 
-import (
-	"fmt"
-)
+import "fmt"
+
+func main() {
+	fmt.Println(title(12))
+
+}
+
+func title(str int) (result int) {
+	result = str
+	defer func() { // defer 后面必须是函数。如果是这中返回值中已经确定了返回名称的形式，defer就可以更改return值，return的顺序是 
+		//先执行return后面的东西 然后执行defer，然后执行return （返回，并停止。）
+		result++ 
+	}()
+	return
+}
+
+
+func title2(str int) int {
+	result := str
+	defer func() { // defer 后面必须是函数。但是这里返回值并没有一个变量，这种形式，defer无法改变返回的结果。
+		result++
+	}()
+	return result
+}
+
+
 //规则：后进先出
 func run1() {
 	defer fmt.Println("world")
