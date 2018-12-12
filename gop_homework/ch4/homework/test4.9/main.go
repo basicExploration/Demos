3d2471ea1138efb2db11745cd2b4f3f20e35268f
googege <googege@icloud.com>
2018-12-12 21:55:19 +0800

    add

--- gop_homework/ch4/homework/test4.9/main.go
+++ gop_homework/ch4/homework/test4.9/main.go
@@ -2,3 +2,29 @@
 // 在第一次调用Scan前先调用input.Split(bufio.ScanWords)函数，这样可以按单词而不是按行输入。
 package main
 
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+func wordfreq() {
+	testMao := make(map[string]int)
+	file, err := os.Open("./tt.txt")
+	fmt.Println(err)
+	input := bufio.NewScanner(file)
+	input.Split(bufio.ScanWords)
+	for input.Scan() {
+		testMao[input.Text()]++
+		//fmt.Println(testMao)
+	}
+	for k,v := range testMao {
+		fmt.Println(len(testMao))
+		fmt.Printf("%s: %% %.3f",k,float32(v*100/len(testMao)))
+	}
+
+}
+
+func main() {
+	wordfreq()
+}
