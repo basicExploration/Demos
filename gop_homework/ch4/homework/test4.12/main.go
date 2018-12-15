1c2e868537a1e987dda9e88a52ca2d8feec3e697
googege <googege@icloud.com>
2018-12-15 21:24:42 +0800

    add test4.11

--- gop_homework/ch4/homework/test4.12/main.go
+++ gop_homework/ch4/homework/test4.12/main.go
@@ -3,3 +3,17 @@
 // 编写一个xkcd工具，使用这些离线索引，打印和命令行输入的检索词相匹配的漫画的URL。
 package main
 
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main() {
+	res, err := http.Get("https://xkcd.com/571/info.0.json")
+	fmt.Println(err)
+	data, err := ioutil.ReadAll(res.Body)
+	fmt.Println(err)
+	fmt.Println(string(data))
+	res.Body.Close()
+}
--- gop_homework/ch5/homework/test5.1/main.go
+++ gop_homework/ch5/homework/test5.1/main.go
@@ -1,2 +1,38 @@
+//练习 5.1： 修改findlinks代码中遍历n.FirstChild链表的部分，将循环调用visit，改成递归调用。
 package main
 
+import (
+	"fmt"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	doc, err := html.Parse(os.Stdin)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "findlinks1: %v\n", err)
+		os.Exit(1)
+	}
+	for _, link := range visit(nil, doc) {
+		fmt.Println(link)
+	}
+}
+
+//!-main
+
+//!+visit
+// visit appends to links each link found in n and returns the result.
+func visit(links []string, n *html.Node) []string {
+	if n.Type == html.ElementNode && n.Data == "a" {
+		for _, a := range n.Attr {
+			if a.Key == "href" {
+				links = append(links, a.Val)
+			}
+		}
+	}
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		links = visit(links, c)
+	}
+	return links
+}
