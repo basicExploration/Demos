de79814a4276d0769c1e19cee1acda8332fb06fc
googege <googege@icloud.com>
2018-12-18 02:35:05 +0800

    Update main.go

--- gop_homework/ch4/homework/test4.14/transfer/main.go
+++ gop_homework/ch4/homework/test4.14/transfer/main.go
@@ -5,7 +5,7 @@ import "net/http"
 func main(){
 
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
-		http.Redirect(writer,request,"https://localhost",http.StatusMovedPermanently)
+		http.Redirect(writer,request,"https://coastroad.net",http.StatusMovedPermanently)
 	})
 	http.ListenAndServe(":80",nil)
 }
