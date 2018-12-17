416b829e34035008df19b6c710e436b6255f390b
googege <googege@icloud.com>
2018-12-17 21:31:16 +0800

    add

---
+++ gop_homework/ch4/homework/test4.14/data.go
@@ -0,0 +1,50 @@
+package main
+
+type Name struct {
+	Login       string `json:"login"`
+	Id          int    `json:"id"`
+	Gravatar_id string `json:"gravatar_id"`
+	Avatar_url  string `json:"avatar_url"`
+	Name        string `json:"name"`
+	Followers   int    `json:"followers"`
+	Html_url    string `json:"html_url"`
+	Company     string `json:"company"`
+	Blog        string `json:"blog"`
+	Bio         string `json:"bio"`
+	Email       string `json:"email"`
+	Location    string `json:"location"`
+}
+
+//{
+//"login": "googege",
+//"id": 42873232,
+//"node_id": "MDQ6VXNlcjQyODczMjMy",
+//"avatar_url": "https://avatars3.githubusercontent.com/u/42873232?v=4",
+//"gravatar_id": "",
+//"url": "https://api.github.com/users/googege",
+//"html_url": "https://github.com/googege",
+//"followers_url": "https://api.github.com/users/googege/followers",
+//"following_url": "https://api.github.com/users/googege/following{/other_user}",
+//"gists_url": "https://api.github.com/users/googege/gists{/gist_id}",
+//"starred_url": "https://api.github.com/users/googege/starred{/owner}{/repo}",
+//"subscriptions_url": "https://api.github.com/users/googege/subscriptions",
+//"organizations_url": "https://api.github.com/users/googege/orgs",
+//"repos_url": "https://api.github.com/users/googege/repos",
+//"events_url": "https://api.github.com/users/googege/events{/privacy}",
+//"received_events_url": "https://api.github.com/users/googege/received_events",
+//"type": "User",
+//"site_admin": false,
+//"name": "3G",
+//"company": "@coastroad",
+//"blog": "https://weibo.com/imgoogege",
+//"location": "China",
+//"email": null,
+//"hireable": true,
+//"bio": "I'M  Gopher & Jser.\r\nFounder & CEO @coastroad",
+//"public_repos": 9,
+//"public_gists": 1,
+//"followers": 1,
+//"following": 19,
+//"created_at": "2018-08-31T16:14:57Z",
+//"updated_at": "2018-12-17T07:11:34Z"
+//}
--- gop_homework/ch4/homework/test4.14/main.go
+++ gop_homework/ch4/homework/test4.14/main.go
@@ -8,6 +8,7 @@ import (
 	"html/template"
 	"io/ioutil"
 	"net/http"
+	"regexp"
 	"strings"
 )
 
@@ -20,27 +21,35 @@ func main() {
 	http.HandleFunc("/", func(writer http.ResponseWriter, request *http.Request) {
 		t, err := template.ParseFiles(HTMLTemplate...)
 		IsErr(err)
-		t.Execute(writer, "https://weibo.com/imgoogege")
+		t.Execute(writer, nil)
 
 	})
+
 	http.HandleFunc("/search", func(writer http.ResponseWriter, request *http.Request) {
 
-		if strings.ToUpper(request.Method) == "POST"{
+		if strings.ToUpper(request.Method) == "POST" {
 			*gID = request.FormValue("gID")
 			fmt.Println(*gID)
-			http.Redirect(writer,request,"/search",304)
-		}else if strings.ToUpper(request.Method) == "GET" {
-			fmt.Println("https://api.github.com/users/"+*gID)
-			res,err := http.Get("https://api.github.com/users/"+*gID)
+			if *gID ==""{
+				http.Redirect(writer, request, "/", http.StatusMovedPermanently)
+			}
+			http.Redirect(writer, request, "/search", http.StatusMovedPermanently)
+			// 这个地方原来是因为后面的那个code然后会有不同的方式。测试 301挺好用。
+			//由于该代码表示页面地址发生了较长久的改变，故Bing[9]和Google[10]等搜索引擎都推荐使用301重定向，以改变搜索引擎中的实际页面地址。
+
+		} else if strings.ToUpper(request.Method) == "GET" {
+			res, err := http.Get("https://api.github.com/users/" + *gID)
 			IsErr(err)
-			da,err := ioutil.ReadAll(res.Body)
+			da, err := ioutil.ReadAll(res.Body)
 			t := new(Name)
-			json.Unmarshal(da,t)
+			json.Unmarshal(da, t)
+			t.Company = regexp.MustCompile("@").ReplaceAllString(t.Company, "")
 			IsErr(err)
-			fmt.Fprint(writer,t.Name,t.Followers)
+			template, err := template.ParseFiles("search.html")
+			IsErr(err)
+			template.Execute(writer, t)
 			res.Body.Close()
 		}
-
 
 	})
 	http.ListenAndServe(":9090", nil)
@@ -51,9 +60,4 @@ func IsErr(err error) {
 	if err != nil {
 		fmt.Println(err)
 	}
-}
-
-type Name struct {
-	Name string `json:"name"`
-	Followers int `json:"followers"`
 }
