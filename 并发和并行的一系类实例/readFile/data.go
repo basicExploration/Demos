7461b5de5c435e98cefc3e514e27acc83d9329b0
googege <googege@icloud.com>
2018-12-20 20:08:17 +0800

    add

---
+++ 并发和并行的一系类实例/readFile/data.go
@@ -0,0 +1,60 @@
+package main
+
+var datas = []string{
+	data,
+	data,
+	data,
+	data,
+	data,
+	data,
+	data,
+	data,
+	data,
+	data,
+	data,
+	data,
+	data,
+	data,
+	data,
+	data,
+	data,
+	data,
+}
+var data = `{
+"b":2,
+"a":1,
+"b":2,
+"c":"bbc",
+"d":'323233',
+"a":1,
+"b":2,
+"c":"bbc",
+"d":'323233',
+"a":1,
+"b":2,
+"c":"bbc",
+"d":'323233',
+"a":1,
+"b":2,
+"c":"bbc",
+"d":'323233',
+"a":1,
+"b":2,
+"c":"bbc",
+"d":'323233',
+"a":1,
+"b":2,
+"c":"bbc",
+"d":'323233',
+"a":1,
+"b":2,
+"c":"bbc",
+"d":'323233',
+}`
+
+type jsonValue struct {
+	A string `json:"a"`
+	B string `json:"b"`
+	C string `json:"c"`
+	D string `json:"d"`
+}
--- 并发和并行的一系类实例/readFile/readfile.go
+++ 并发和并行的一系类实例/readFile/readfile.go
@@ -1,4 +1,41 @@
 // io 型 bound
 package main
 
+import (
+	"encoding/json"
+	"sync"
+	"time"
+)
 
+func read(s string) {
+	document := new(jsonValue)
+	time.Sleep(time.Millisecond)
+	json.Unmarshal([]byte(s), document)
+}
+
+func find(doc []string) {
+	for _, v := range doc {
+		read(v)
+	}
+}
+
+func findCon(cpu int, doc []string) {
+	var synv sync.WaitGroup
+	ch := make(chan string, len(doc))
+	for i := 0; i < len(doc); i++ {
+		ch <- doc[i]
+	}
+	close(ch)
+	synv.Add(cpu)
+	for i := 0; i < cpu; i++ {
+		go func() {
+			for v := range ch {
+				read(v)
+			}
+			synv.Done()
+		}()
+	}
+
+	synv.Wait()
+
+}
