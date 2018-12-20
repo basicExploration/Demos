20363ff85c4a6057d42e355a7a8c6e7d38384d72
googege <googege@icloud.com>
2018-12-20 10:16:09 +0800

    add

--- 并发和并行的一系类实例/add/add_test.go
+++ 并发和并行的一系类实例/add/add_test.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"runtime"
 	"testing"
 )
@@ -19,13 +20,18 @@ func BenchmarkAdd(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		add(num)
 	}
+	fmt.Println(len(num))// 经过计算 len是
 }
 
 func BenchmarkNadd(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		nadd(runtime.NumCPU(),num)
 	}
+	fmt.Println(len(num))
 }
 
 
-// 只有这种数量级的时候才能战胜单线程。
+// 只有这种数量级的时候才能战胜单线程。 也就是 150k的时候 并发性能更好，如果是数量级非常小 比如 几千几万的时候 并发（并发什么时候都是不行的，
+// 因为这是cpu bound）并行也是不行的。只有数量级非常大并且非常消耗计算量的时候 并行很厉害，但是也不会是跨越数量级的厉害。
+
+//综上所述 cpu型 bound 并行 > 顺序执行 > 并发
