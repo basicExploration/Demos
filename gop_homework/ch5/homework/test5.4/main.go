98a18b945872bc45d15d97edb5d33e95c772ce5c
googege <googege@icloud.com>
2018-12-24 18:15:00 +0800

    add

--- gop_homework/ch5/homework/test5.4/main.go
+++ gop_homework/ch5/homework/test5.4/main.go
@@ -1,2 +1,21 @@
 //练习 5.4： 扩展visit函数，使其能够处理其他类型的结点，如images、scripts和style sheets。
 package main
+
+import (
+	"fmt"
+)
+
+func main(){
+	s := make([]string,1,10)
+	s = []string{
+		"1",
+	}
+	visit(s)
+	fmt.Println(s)
+
+}
+
+func visit(s []string){
+ s =append(s,"44334")
+ fmt.Println(&s)
+}
