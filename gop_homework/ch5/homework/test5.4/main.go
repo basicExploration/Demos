d412cd31ed09b6dcf4735ebfe54a6eeec6299301
googege <googege@icloud.com>
2018-12-24 21:31:16 +0800

    Update main.go

--- gop_homework/ch5/homework/test5.4/main.go
+++ gop_homework/ch5/homework/test5.4/main.go
@@ -9,7 +9,7 @@ import (
 
 func main() {
 	data := make([]string, 0)
-	s := "<p>43433433232</p><p>胡家齐这是一个很累大的撞击</p><a href='/123'>32</a><script>fdsfd</script>"
+	s := "<p>43433433232</p><style href='./css'>胡家齐这是一个很累大的撞击</style><a href='/123' style='fdsfdsf;'>32</a><script href='./js'>fdsfd</script><img href='4343' >"
 	node, _ := html.Parse(strings.NewReader(s))
 	visit(&data, node)
 	fmt.Println(data)
@@ -17,12 +17,26 @@ func main() {
 
 func visit(s *[]string, n *html.Node) {
 
-	if n.Type == html.TextNode {
+	if n.Type == html.ElementNode  {
+		switch n.Data {
+		case "img","image":
+			for	_,v :=  range n.Attr {
+				fmt.Println(v.Val,v.Key,v.Namespace)
+		}
+		case "script":
+			for	_,v :=  range n.Attr {
+				fmt.Println(v.Val,v.Key,v.Namespace)
+			}
+		case "style":
+			for	_,v :=  range n.Attr {
+				fmt.Println(v.Val,v.Key,v.Namespace,n.Attr)
+			}
 
-			fmt.Println(n.Data)
-
-
+		}
 	}
+	//if n.Type == html.TextNode {
+	//	fmt.Println(n.Data)
+	//}
 	for g := n.FirstChild; g != nil; g = g.NextSibling {
 		visit(s, g)
 	}
