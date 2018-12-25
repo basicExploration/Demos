797156f13306b93d9598d124bbe9fb7c36665a35
googege <googege@icloud.com>
2018-12-25 15:56:29 +0800

    Update main.go

--- gop_homework/ch5/homework/test5.5/main.go
+++ gop_homework/ch5/homework/test5.5/main.go
@@ -2,10 +2,13 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
 	"golang.org/x/net/html"
-	"net/http"
+	"strings"
 )
+
+var ima, wor int
 
 func main() {
 
@@ -15,19 +18,44 @@ func main() {
 //CountWordsAndImages does an HTTP GET request for the HTML
 //document url and returns the number of words and images in it.
 func CountWordsAndImages(url string) (words, images int, err error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return
-	}
-	doc, err := html.Parse(resp.Body)
-	resp.Body.Close()
+	s := "<p>p1</p><a>a1<a>a12</a></a><p>p2</p>"
+	re := strings.NewReader(s)
+	doc, err := html.Parse(re) // resp.body已经实现了reader接口直接传进去即可。
 	if err != nil {
 		err = fmt.Errorf("parsing HTML: %s", err)
 		return
 	}
 	words, images = countWordsAndImages(doc)
+	fmt.Printf("words:%d,images:%d\n", words, images)
 	return
 }
-func countWordsAndImages(n *html.Node) (words, images int) {
+func countWordsAndImages(n *html.Node) (int, int) {
+	if n.Type == html.ElementNode && n.Data == "img" {
+		ima++
+	}
+	if n.Type == html.TextNode {
+		fmt.Println(n.Data)
+		sRead := strings.NewReader(n.Data)
+		scan := bufio.NewScanner(sRead)
+		scan.Split(bufio.ScanWords)
+		if scan.Scan() {
+			wor++
+		}
+	}
+	for i := n.FirstChild; i != nil; i = i.NextSibling {
+		wor, ima = countWordsAndImages(i)
+	}
+	return wor, ima
 
 }
+
+//p1
+//a1
+//a12
+//p2
+//words:4,images:0
+
+//这个 出现的过程表示了 首先查找的节点的规律是 查找p然后看看p有没有子节点，没有接查它的兄弟，然后全部没有会返回nil
+//也就是说顺序是
+//
+// 自己-子女-兄弟。
