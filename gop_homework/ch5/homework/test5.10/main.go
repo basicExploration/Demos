6f5b2c8dd592530656280f8832b5533eb4ea13dc
googege <googege@icloud.com>
2018-12-26 20:40:02 +0800

    增加test5.12 测试能用。
    减少了全局变量改为了局部变量后，更加优雅而且减少了变量污染。

--- gop_homework/ch5/homework/test5.10/main.go
+++ gop_homework/ch5/homework/test5.10/main.go
@@ -1,18 +1,22 @@
 //练习5.10： 重写topoSort函数，用map代替切片并移除对key的排序代码。验证结果的正确性（结果不唯一）。
+// 深度优先算法
+// 广度 优先算法
 package main
 
 import (
 	"fmt"
-	"sort"
+	"gopl.io/ch5/links"
+	"log"
 )
 
+//深度
 func topoSort(m map[string][]string) []string {
 	var order []string
 	seen := make(map[string]bool)
 	var visitAll func(items []string)
 	visitAll = func(items []string) {
 		for _, item := range items {
-			if !seen[item] {
+			if !seen[item] { // 如果还没有定义过得 bool初始值是false
 				seen[item] = true
 				visitAll(m[item])
 				order = append(order, item)
@@ -20,23 +24,68 @@ func topoSort(m map[string][]string) []string {
 		}
 	}
 	var keys []string
-	for key := range m {
-		keys = append(keys, key)
+	for _, v := range m {
+		keys = append(keys, v...)
 	}
-	sort.Strings(keys)
 	visitAll(keys)
 
-	var t func()int
-	t = func() int {
-		return 12
-	}
-	t()
+	//var t func() int
+	//t = func() int {
+	//	return 12
+	//}
+	//t()
 	return order
 }
 
+//--------
+//广度
+
+func breadthFirst(f func(item string) []string, worklist []string) {
+	seen := make(map[string]bool)
+	for len(worklist) > 0 {
+		items := worklist
+		worklist = nil
+		for _, item := range items {
+			if !seen[item] {
+				seen[item] = true
+				worklist = append(worklist, f(item)...)
+			}
+		}
+	}
+}
+
+//!-breadthFirst
+
+//!+crawl
+func crawl(url string) []string {
+	fmt.Println(url)
+	list, err := links.Extract(url)
+	if err != nil {
+		log.Print(err)
+	}
+	return list
+}
 
 func main() {
-	fmt.Println(topoSort(map[string][]string{
-		"1": []string{"1", "2", "3"},
-	}))
+	t := map[string][]string{
+		"1": {"1", "2", "3","332"},
+		"2": {"2", "23", "32", "554"},
+	}
+	fmt.Println(topoSort(t))
 }
+
+
+//func tt(){
+//
+//	t := []int{
+//		1,3,3,5,3,
+//	}
+//	d := [][]int{
+//		{1,3,23,},
+//	}
+//	for _,v := range t  {
+//		for _,d := range d[v]{
+//			fmt.Println(d)
+//		}
+//	}
+//}
--- gop_homework/ch5/homework/test5.12/main.go
+++ gop_homework/ch5/homework/test5.12/main.go
@@ -2,3 +2,89 @@
 // 将它们修改为匿名函数，使其共享outline中的局部变量。
 package main
 
+import (
+	"fmt"
+	"net/http"
+	"os"
+
+	"golang.org/x/net/html"
+)
+
+func main() {
+	for _, url := range os.Args[1:] {
+		outline(url)
+	}
+}
+
+func outline(url string) error {
+	depth := 0
+	var startElement func(n *html.Node, depth int)
+	startElement = func(n *html.Node, depth int) {
+		if n.Type == html.ElementNode {
+			fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+			depth++
+		}
+	}
+	var endElement func(n *html.Node, depth int)
+	endElement = func(n *html.Node, depth int) {
+		if n.Type == html.ElementNode {
+			depth--
+			fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+		}
+	}
+	resp, err := http.Get(url)
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	doc, err := html.Parse(resp.Body)
+	if err != nil {
+		return err
+	}
+
+	//!+call
+	forEachNode(doc, depth, startElement, endElement)
+	//!-call
+
+	return nil
+}
+
+//!+forEachNode
+// forEachNode calls the functions pre(x) and post(x) for each node
+// x in the tree rooted at n. Both functions are optional.
+// pre is called before the children are visited (preorder) and
+// post is called after (postorder).
+func forEachNode(n *html.Node, depth int, pre, post func(*html.Node, int)) {
+	if pre != nil {
+		pre(n, depth)
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		forEachNode(c, depth, pre, post)
+	}
+
+	if post != nil {
+		post(n, depth)
+	}
+}
+
+//!-forEachNode
+
+//!+startend
+
+//func startElement(n *html.Node) {
+//	if n.Type == html.ElementNode {
+//		fmt.Printf("%*s<%s>\n", depth*2, "", n.Data)
+//		depth++
+//	}
+//}
+//
+//func endElement(n *html.Node) {
+//	if n.Type == html.ElementNode {
+//		depth--
+//		fmt.Printf("%*s</%s>\n", depth*2, "", n.Data)
+//	}
+//}
+
+//!-startend
