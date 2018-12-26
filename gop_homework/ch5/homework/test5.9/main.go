bf698cf9b2dae620c6d81be0fd87ce91e4e699d1
googege <googege@icloud.com>
2018-12-26 16:14:55 +0800

    add 5.9

--- gop_homework/ch5/homework/test5.9/main.go
+++ gop_homework/ch5/homework/test5.9/main.go
@@ -1,2 +1,16 @@
+//练习 5.9： 编写函数expand，将s中的"foo"替换为f("foo")的返回值。
 package main
 
+import "fmt"
+
+func expand(s string, f func(string) string) string {
+	return f(s)
+}
+
+func main() {
+	s := expand("12", func(s string) string {
+		s += "."
+		return s
+	})
+	fmt.Println(s)
+}
