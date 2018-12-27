0614a131287c68193a6a6cba0545b477e8ad070c
googege <googege@icloud.com>
2018-12-27 14:57:25 +0800

    Update main.go

--- gop_homework/ch5/homework/test5.15/main.go
+++ gop_homework/ch5/homework/test5.15/main.go
@@ -1,7 +1,9 @@
 //练习5.15： 编写类似sum的可变参数函数max和min。考虑不传参时，max和min该如何处理，再编写至少接收1个参数的版本。
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func sum(vals ...int) int {
 	total := 0
@@ -60,4 +62,12 @@ func minLeastOne(vals ...int) int {
 func main() {
 	fmt.Println(max(1, 2, 3, 4, 53, 5))
 	fmt.Println(min(12, 2334, 221, 2))
+	 //var _ io.Writer = (*dd)(nil) // 测试代码
 }
+
+//type dd struct{// 测试代码
+//
+//}
+//func(d1 *dd)Write(p []byte)(int,error){
+//	return 0,nil
+//}
