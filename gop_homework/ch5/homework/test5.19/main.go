603c89c955ca9cdc1901d8983ffa2504c38a2355
googege <googege@icloud.com>
2018-12-27 20:41:25 +0800

    第五章出去todo 搞定

--- gop_homework/ch5/homework/test5.19/main.go
+++ gop_homework/ch5/homework/test5.19/main.go
@@ -1,3 +1,22 @@
 //练习5.19： 使用panic和recover编写一个不包含return语句但能返回一个非零值的函数。
 package main
 
+import "fmt"
+
+func tt(){
+	defer func() {
+		if e := recover();e != nil {
+			fmt.Println(e)
+		}
+	}()
+	dd()
+}
+
+
+func dd(){
+	panic("12")
+}
+
+func main(){
+	tt()
+}
