986e6050abee1f1b500635b402e62bff0b642881
googege <googege@icloud.com>
2018-12-31 00:34:50 +0800

    u

--- gop_homework/ch7/homework/test7.4/main.go
+++ gop_homework/ch7/homework/test7.4/main.go
@@ -1,1 +1,38 @@
+//练习 7.4： strings.NewReader函数通过读取一个string参数返回一个满足io.Reader接口类型的值
+//(和其它值) 实现一个简单版本的NewReader，并用它来构造一个接收字符串输入的HTML解析器（§5.2）
 package main
+
+import (
+	"fmt"
+	"io"
+	"io/ioutil"
+)
+
+type File struct {
+	s        string
+	i        int64 // current reading index
+	prevRune int
+}
+
+func (r *File) Read(p []byte) (n int, err error) { // 返回写入的字节数和返回错误。
+	if r.i >= int64(len(r.s)) {// 一定要有EOF的判断否则使用其他读取reader的函数就会卡死。
+		return 0, io.EOF
+	}
+	r.prevRune = -1
+	n = copy(p, r.s[r.i:])
+	r.i += int64(n)
+	return
+}
+
+func main() {
+	b, _ := ioutil.ReadAll(NewReader("12"))
+	fmt.Println(b)
+}
+
+func NewReader(s string) io.Reader {
+	return &File{
+		s,
+		0,
+		-1,
+	}
+}
--- gop_homework/ch7/homework/test7.5/main.go
+++ gop_homework/ch7/homework/test7.5/main.go
@@ -1,1 +1,4 @@
+//练习 7.5： io包里面的LimitReader函数接收一个io.Reader接口类型的r和字节数n，
+// 并且返回另一个从r中读取字节但是当读完n个字节后就表示读到文件结束的Reader。
+// 实现这个LimitReader函数：
 package main
