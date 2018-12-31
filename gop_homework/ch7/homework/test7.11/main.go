8fa889a3a4c797e81375ea4882fcb5932e5788c1
googege <googege@icloud.com>
2018-12-31 19:47:17 +0800

    up

--- gop_homework/ch7/homework/test7.11/main.go
+++ gop_homework/ch7/homework/test7.11/main.go
@@ -1,1 +1,28 @@
+//练习 7.11： 增加额外的handler让客服端可以创建，读取，更新和删除数据库记录。
+// 例如，一个形如 /update?item=socks&price=6 的请求会更新库存清单里一个货品的价格
+// 并且当这个货品不存在或价格无效时返回一个错误值。（注意：这个修改会引入变量同时更新的问题）
 package main
+
+import (
+	"fmt"
+	"net/http"
+	"strings"
+)
+
+type a func(w http.ResponseWriter, r *http.Request)
+
+func (a1 a) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	a1(w, r)
+}
+func main() {
+	var a1 a
+	a1 = func(w http.ResponseWriter, r *http.Request) {
+		if strings.ToUpper(r.URL.Path) == "/UPDATE"{
+			fmt.Println("d")
+			item := r.FormValue("item")
+			price := r.FormValue("price")
+			w.Write([]byte(item + price))
+		}
+	}
+	http.ListenAndServe(":8090", a1)
+}
--- gop_homework/ch7/homework/test7.12/main.go
+++ gop_homework/ch7/homework/test7.12/main.go
@@ -1,1 +1,2 @@
+//练习 7.12： 修改/list的handler让它把输出打印成一个HTML的表格而不是文本。html/template包(§4.6)可能会对你有帮助。
 package main
