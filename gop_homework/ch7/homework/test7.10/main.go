38d572ddeecd02d1419c66d6a43073eb3fd75469
googege <googege@icloud.com>
2018-12-31 16:07:22 +0800

    update test7.9

--- gop_homework/ch7/homework/test7.10/main.go
+++ gop_homework/ch7/homework/test7.10/main.go
@@ -1,1 +1,4 @@
+//练习 7.10： sort.Interface类型也可以适用在其它地方。编写一个IsPalindrome(s sort.Interface) bool
+// 函数表明序列s是否是回文序列，换句话说反向排序不会改变这个序列。
+// 假设如果!s.Less(i, j) && !s.Less(j, i)则索引i和j上的元素相等。
 package main
--- gop_homework/ch7/homework/test7.9/main.go
+++ gop_homework/ch7/homework/test7.9/main.go
@@ -1,1 +1,82 @@
+//练习 7.9： 使用html/template包 (§4.6) 替代printTracks将tracks展示成一个HTML表格。
+// 将这个解决方案用在前一个练习中，让每次点击一个列的头部产生一个HTTP请求来排序这个表格。
 package main
+
+import (
+	"fmt"
+	"html/template"
+	"os"
+	"time"
+)
+
+type Track struct {
+	Title  string
+	Artist string
+	Album  string
+	Year   time.Time
+	Length int
+}
+
+func printTracks(tracks []*Track) {
+	va := `
+<table>
+<tr>
+<td>
+{{.Title}}
+</td>
+<td>
+{{.Artist}}
+</td>
+<td>
+{{.Album}}
+</td>
+<td>
+{{.Year}}
+</td>
+<td>
+{{.Length}}
+</td>
+</tr>
+<tr>
+<td>
+</td>
+<td>
+</td>
+<td>
+</td>
+<td>
+</td>
+<td>
+</td>
+</tr>
+
+</table>
+
+
+`
+	t1, err := template.New("foo").Parse(va)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	//const format = "%v\t%v\t%v\t%v\t%v\t\n"
+	//tw := new(tabwriter.Writer).Init(os.Stdout, 0, 8, 2, ' ', 0)
+	//fmt.Fprintf(tw, format, "Title", "Artist", "Album", "Year", "Length")
+	//fmt.Fprintf(tw, format, "-----", "------", "-----", "----", "------")
+	for _, t := range tracks {
+		err = t1.ExecuteTemplate(os.Stdout, "foo", t)
+		//fmt.Fprintf(tw, format, t.Title, t.Artist, t.Album, t.Year, t.Length)
+	}
+	//tw.Flush() // calculate column widths and print table
+}
+
+func main() {
+	printTracks([]*Track{
+		&Track{
+			"12", "233", "2332", time.Now(), 23,
+		},
+		&Track{
+			"fdfdfd", "2fsdfsd33", "233dsfsdf2", time.Now(), 665,
+		},
+	})
+}
