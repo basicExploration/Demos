b15489918234e280d2c76f95c88cf8cb1a7d708c
googege <googege@icloud.com>
2018-12-31 17:31:52 +0800

    Update main.go

--- gop_homework/ch7/homework/test7.10/main.go
+++ gop_homework/ch7/homework/test7.10/main.go
@@ -2,3 +2,18 @@
 // 函数表明序列s是否是回文序列，换句话说反向排序不会改变这个序列。
 // 假设如果!s.Less(i, j) && !s.Less(j, i)则索引i和j上的元素相等。
 package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+// 函数应该只使用接口，然后行使基础权利。然后传入动态实际值，这样就可以实施真实值了。
+func IsPalindrome(s sort.Interface, i int, j int) bool {
+	if !s.Less(i, j) && !s.Less(j, i) {
+		fmt.Println("则索引i和j上的元素相等")
+		return true
+
+	}
+	return false
+}
