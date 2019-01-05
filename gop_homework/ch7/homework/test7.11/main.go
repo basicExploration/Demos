5cb8d6805c75affe4c1129fe08bf344aba4c95af
googege <googege@icloud.com>
2019-01-05 21:53:49 +0800

    Update main.go

--- gop_homework/ch7/homework/test7.11/main.go
+++ gop_homework/ch7/homework/test7.11/main.go
@@ -1,28 +1,56 @@
 //练习 7.11： 增加额外的handler让客服端可以创建，读取，更新和删除数据库记录。
-// 例如，一个形如 /update?item=socks&price=6 的请求会更新库存清单里一个货品的价格
-// 并且当这个货品不存在或价格无效时返回一个错误值。（注意：这个修改会引入变量同时更新的问题）
+// 例如，一个形如 /update?name=1&num=1 的请求会更新一个人的name和num
+// 并且当这个人的名字无效时返回一个错误值。
 package main
 
 import (
+	"database/sql"
 	"fmt"
+	_ "github.com/go-sql-driver/mysql"
 	"net/http"
+	"os"
 	"strings"
 )
 
-type a func(w http.ResponseWriter, r *http.Request)
+var (
+	db  *sql.DB
+	err error
+)
 
-func (a1 a) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	a1(w, r)
-}
-func main() {
-	var a1 a
-	a1 = func(w http.ResponseWriter, r *http.Request) {
-		if strings.ToUpper(r.URL.Path) == "/UPDATE"{
-			fmt.Println("d")
-			item := r.FormValue("item")
-			price := r.FormValue("price")
-			w.Write([]byte(item + price))
-		}
+func init() {
+	db, err = sql.Open("mysql", "#")
+	if err != nil {
+		fmt.Println("连接数据库发生错误：", err)
+		os.Exit(1)
 	}
-	http.ListenAndServe(":8090", a1)
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if strings.ToUpper(r.URL.Path) == "/" {
+			var sqldata string
+			row, err := db.Query("SELECT name FROM room")
+			if err != nil {
+				fmt.Println(err)
+			}
+			for row.Next() {
+				t := new(string)
+				err = row.Scan(t)
+				if err != nil {
+					fmt.Println(err)
+				}
+				sqldata += *t
+			}
+			fmt.Println("d")
+			fmt.Println("ceshi", sqldata)
+			w.Write([]byte(sqldata))
+		}
+	})
+	http.HandleFunc("/up", func(w http.ResponseWriter, r *http.Request) {
+		name := r.FormValue("name")
+		number := r.FormValue("number")
+		db.QueryRow("UPDATE room SET name=?", name).Scan(nil)
+		db.QueryRow("UPDATE room SET number=?", number).Scan(nil)
+	})
+	http.ListenAndServe(":8090", nil)
 }
