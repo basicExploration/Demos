9655870c39d260abe0b6572838607e6e76d441be
googege <googege@icloud.com>
2019-01-06 11:34:09 +0800

    从form改成了fetch ajax的模式。

--- gop_homework/ch7/homework/test7.16/main.go
+++ gop_homework/ch7/homework/test7.16/main.go
@@ -1,21 +1,26 @@
 //练习 7.16： 编写一个基于web的计算器程序。
+
+
+// 关注几点 1， 使用前端往后端传输只能用post方法，前端从后端取数据只能用get，前端使用本方案的常规模式只能传过来都是string没办法传过来int数据。
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"html/template"
+	"io/ioutil"
 	"net/http"
 	"strconv"
 )
 
 type result struct {
-	One    int
-	Two    int
-	Fuhao  string
-	Dengyu string
-	Resu   int
+	One    string `json:"one"`
+	Two    string `json:"two"`
+	Fuhao  string `json:"fuhao"`
+	Dengyu string `json:"dengyu"`
+	Resu   int `json:"resu"`
 }
-
+var ret  = make(map[string]int)
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println(r.Method)
@@ -25,37 +30,36 @@ func main() {
 		}
 
 		if r.Method == "POST" {
-			var d  = new(string)
-			r.ParseForm()
-			r.Body.Read([]byte(*d))
-			fmt.Println("显示数据",*d)
-			one := r.FormValue("one")
-			o, _ := strconv.Atoi(one)
-			two := r.FormValue("two")
-			t, _ := strconv.Atoi(two)
-			huhao := r.FormValue("fuhao")
-			var res = result{
-				o, t, huhao, "=", 0,
-			}
-			switch huhao {
+			data,_ := ioutil.ReadAll(r.Body)
+			res := new(result)
+			fmt.Println("看看data中有什么？",string(data))
+			json.Unmarshal(data,res)
+			fmt.Println(res)
+			one,_ := strconv.Atoi(res.One)
+			two,_ := strconv.Atoi(res.Two)
+			switch res.Fuhao {
 			case "jia":
-				res.Resu = o + t
+
+				res.Resu = one + two
 				res.Fuhao = "+"
 			case "jian":
-				res.Resu = o - t
+				res.Resu = one - two
 				res.Fuhao = "-"
 			case "cheng":
-				res.Resu = o * t
+				res.Resu = one * two
 				res.Fuhao = "x"
 			case "chu":
-				res.Resu = o / t
+				res.Resu = one / two
 				res.Fuhao = "÷"
 			default:
 				res.Resu = 0
 			}
-			tem, _ := template.ParseFiles("./main.html")
-			tem.Execute(w, res)
+			ret["name"] = res.Resu
 		}
+	})
+	http.HandleFunc("/s", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprint(w,ret["name"])
+
 	})
 	fmt.Println(http.ListenAndServe(":8090", nil))
 }
