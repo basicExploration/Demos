e4e505cbcbf0602ee8cd9974a8792a5dc52f0ffd
googege <googege@icloud.com>
2019-01-06 10:24:46 +0800

    update

--- gop_homework/ch7/homework/test7.15/main.go
+++ gop_homework/ch7/homework/test7.15/main.go
@@ -1,3 +1,19 @@
 //练习 7.15： 编写一个从标准输入中读取一个单一表达式的程序，用户及时地提供对于任意变量的值，
 // 然后在结果环境变量中计算表达式的值。优雅的处理所有遇到的错误。
 package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os"
+)
+
+func main(){
+	data,err := ioutil.ReadAll(os.Stdin)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(data)
+	//这个练习是什么鬼意思？
+}
+
--- gop_homework/ch7/homework/test7.16/main.go
+++ gop_homework/ch7/homework/test7.16/main.go
@@ -1,2 +1,57 @@
 //练习 7.16： 编写一个基于web的计算器程序。
 package main
+
+import (
+	"fmt"
+	"html/template"
+	"net/http"
+	"strconv"
+)
+
+type result struct {
+	One    int
+	Two    int
+	Fuhao  string
+	Dengyu string
+	Resu   int
+}
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Println(r.Method)
+		if r.Method == "GET" {
+			tem, _ := template.ParseFiles("./main.html")
+			tem.Execute(w, nil)
+		}
+
+		if r.Method == "POST" {
+			one := r.FormValue("one")
+			o, _ := strconv.Atoi(one)
+			two := r.FormValue("two")
+			t, _ := strconv.Atoi(two)
+			huhao := r.FormValue("fuhao")
+			var res = result{
+				o, t, huhao, "=", 0,
+			}
+			switch huhao {
+			case "jia":
+				res.Resu = o + t
+				res.Fuhao = "+"
+			case "jian":
+				res.Resu = o - t
+				res.Fuhao = "-"
+			case "cheng":
+				res.Resu = o * t
+				res.Fuhao = "x"
+			case "chu":
+				res.Resu = o / t
+				res.Fuhao = "÷"
+			default:
+				res.Resu = 0
+			}
+			tem, _ := template.ParseFiles("./main.html")
+			tem.Execute(w, res)
+		}
+	})
+	fmt.Println(http.ListenAndServe(":8090", nil))
+}
