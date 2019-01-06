db51b1dd1c97fe340c7fbf3905e5243749a21982
googege <googege@icloud.com>
2019-01-06 09:36:34 +0800

    add

--- gop_homework/ch7/homework/test7.13/main.go
+++ gop_homework/ch7/homework/test7.13/main.go
@@ -1,2 +1,8 @@
 //练习 7.13： 为Expr增加一个String方法来打印美观的语法树。当再一次解析的时候，检查它的结果是否生成相同的语法树。
 package main
+
+import "fmt"
+
+type Expr interface {
+	fmt.Stringer
+}
--- gop_homework/ch7/homework/test7.14/main.go
+++ gop_homework/ch7/homework/test7.14/main.go
@@ -1,3 +1,21 @@
 //练习 7.14： 定义一个新的满足Expr接口的具体类型并且提供一个新的操作例如对它运算单元中的最小值的计算。
 // 因为Parse函数不会创建这个新类型的实例，为了使用它你可能需要直接构造一个语法树（或者继承parser接口）。
 package main
+
+
+func main(){
+
+}
+
+
+type str struct {
+
+}
+
+func(s *str)Eval(env Env) float64 {
+	return 1.0
+}
+
+func(s *str)Check(vars map[Var]bool) error{
+return nil
+}
