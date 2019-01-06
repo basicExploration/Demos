4f9dbba7cf137296cca1955becff99a9a0500118
googege <googege@icloud.com>
2019-01-06 15:31:00 +0800

    add

--- gop_homework/ch7/homework/test7.18/main.go
+++ gop_homework/ch7/homework/test7.18/main.go
@@ -1,3 +1,18 @@
 //练习 7.18： 使用基于标记的解码API，编写一个可以读取任意XML文档和构造这个文档所代表的普通节点树的程序。
 // 节点有两种类型：CharData节点表示文本字符串，和 Element节点表示被命名的元素和它们的属性。每一个元素节点有一个字节点的切片。
 package main
+
+import (
+	"encoding/xml"
+)
+
+type Node interface{} // CharData or *Element
+
+type CharData string
+
+type Element struct {
+	Type     xml.Name // 节点名称
+	Attr     []xml.Attr// 节点属性
+	Children []Node// 子节点。
+}
+
