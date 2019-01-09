1584b5198d0dc2d1e81cbf22f6e1ce63bb31949e
googege <googege@icloud.com>
2019-01-09 21:46:35 +0800

    add

--- gop_homework/ch9/homework/test9.4/main.go
+++ gop_homework/ch9/homework/test9.4/main.go
@@ -1,3 +1,20 @@
 //练习 9.4: 创建一个流水线程序，支持用channel连接任意数量的goroutine，
 // 在跑爆内存之前，可以创建多少流水线阶段？一个变量通过整个流水线需要用多久
 package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	ch := make(chan int)
+	for i :=0;i < 3;i++{
+		go func(i int) {
+			fmt.Println()
+			ch <- i
+		}(i)
+	}
+	fmt.Println("d:",<-ch)
+	time.Sleep(1e10)
+}
