3e887be0f71704e720bfc519f13ed181ee73148b
googege <googege@icloud.com>
2019-01-09 20:56:37 +0800

    up

--- gop_homework/ch8/homework/test8.12/main.go
+++ gop_homework/ch8/homework/test8.12/main.go
@@ -3,4 +3,30 @@
 // 并且在entering和leaving的channel中记录客户端的名字。
 package main
 
+type client chan<- string // an outgoing message channel
 
+var (
+	entering = make(chan client)
+	leaving  = make(chan client)
+	messages = make(chan string) // all incoming client messages
+)
+
+func broadcaster() {
+	clients := make(map[client]bool) // all connected clients
+	for {
+		select {
+		case msg := <-messages:
+			// Broadcast incoming message to all
+			// clients' outgoing message channels.
+			for cli := range clients {
+				cli <- msg
+			}
+		case cli := <-entering:
+			clients[cli] = true
+
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli)
+		}
+	}
+}
--- gop_homework/ch8/homework/test8.13/main.go
+++ gop_homework/ch8/homework/test8.13/main.go
@@ -2,6 +2,26 @@
 // 。提示：可以在其它goroutine中调用conn.Close()来解除Read调用，就像input.Scanner()所做的那样。
 package main
 
-func main(){
+import "time"
 
+func broadcaster() {
+	clients := make(map[client]bool) // all connected clients
+	for {
+		select {
+		case <-time.After(time.Minute * 5):
+			return // 已经延迟5分钟了，可以关闭这个连接了。
+		case msg := <-messages:
+			// Broadcast incoming message to all
+			// clients' outgoing message channels.
+			for cli := range clients {
+				cli <- msg
+			}
+		case cli := <-entering:
+			clients[cli] = true
+
+		case cli := <-leaving:
+			delete(clients, cli)
+			close(cli)
+		}
+	}
 }
