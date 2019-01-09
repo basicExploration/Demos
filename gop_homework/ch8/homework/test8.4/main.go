7fed948d4da55d512cca8fc73df7f2cb80df191d
googege <googege@icloud.com>
2019-01-09 10:50:21 +0800

    Update main.go

--- gop_homework/ch8/homework/test8.4/main.go
+++ gop_homework/ch8/homework/test8.4/main.go
@@ -4,3 +4,54 @@
 // 验证一下你的修改版netcat3客户端会一直等待所有的并发“喊叫”完成，
 // 即使是在标准输入流已经关闭的情况下。
 package main
+
+import (
+	"bufio"
+	"fmt"
+	"log"
+	"net"
+	"strings"
+	"sync"
+	"time"
+)
+var sy sync.WaitGroup
+func echo(c net.Conn, shout string, delay time.Duration) {
+	defer sy.Done()
+	fmt.Fprintln(c, "\t", strings.ToUpper(shout))
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", shout)
+	time.Sleep(delay)
+	fmt.Fprintln(c, "\t", strings.ToLower(shout))
+}
+
+//!+
+func handleConn(c net.Conn) {
+	input := bufio.NewScanner(c)
+	for input.Scan() {
+		sy.Add(1)
+		go echo(c, input.Text(), 1*time.Second)
+	}
+	// NOTE: ignoring potential errors from input.Err()
+	c.Close()
+}
+
+//!-
+
+func main() {
+
+	l, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		conn, err := l.Accept()
+		if err != nil {
+			log.Print(err) // e.g., connection aborted
+			continue
+		}
+		go handleConn(conn)
+	}
+	sy.Wait()
+}
+
+
