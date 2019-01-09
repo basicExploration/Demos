9b6e2ce934dfefa0aeedd0521b0a1c1d34095945
googege <googege@icloud.com>
2019-01-09 19:08:42 +0800

    Update main.go

--- gop_homework/ch8/homework/test8.9/main.go
+++ gop_homework/ch8/homework/test8.9/main.go
@@ -1,2 +1,25 @@
 //练习 8.9： 编写一个du工具，每隔一段时间将root目录下的目录大小计算并显示出来。
 package main
+
+import (
+	"fmt"
+	"os/exec"
+	"time"
+)
+
+func main() {
+	du()
+}
+
+func du() {
+	for {
+		cmd := exec.Command("ls", "-i", "/")
+		data, err := cmd.Output()
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(string(data))
+		time.Sleep(time.Second * 3)
+	}
+
+}
