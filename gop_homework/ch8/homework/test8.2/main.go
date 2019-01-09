dc21b0fdfd0981d12a5db7b245ee85c2502d78b5
googege <googege@icloud.com>
2019-01-09 09:44:35 +0800

    Update main.go

--- gop_homework/ch8/homework/test8.2/main.go
+++ gop_homework/ch8/homework/test8.2/main.go
@@ -2,3 +2,24 @@
 // ls来列出目录内文件，get和send来传输文件，close来关闭连接。
 // 你可以用标准的ftp命令来作为客户端，或者也可以自己实现一个。
 package main
+
+import (
+	"github.com/goftp/server"
+)
+
+type MyDriverFactory struct {
+
+}
+func(m MyDriverFactory) NewDriver() (server.Driver, error){
+	return nil,nil
+}
+func main() {
+	factory := &MyDriverFactory{}
+	opts    := &server.ServerOpts{
+		Factory: factory,
+		Port: 2000,
+		Hostname: "127.0.0.1",
+	}
+	server  := server.NewServer(opts)
+	server.ListenAndServe()
+}
