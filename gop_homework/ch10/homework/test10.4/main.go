e3dd57dfad96c24eab29b17a26ef5f4555ed1ec6
googege <googege@icloud.com>
2019-01-10 21:37:16 +0800

    Update main.go

--- gop_homework/ch10/homework/test10.4/main.go
+++ gop_homework/ch10/homework/test10.4/main.go
@@ -1,3 +1,48 @@
 //练习 10.4： 创建一个工具，根据命令行指定的参数，报告工作区所有依赖包指定的其它包集合。提示：
 // 你需要运行go list命令两次，一次用于初始化包，一次用于所有包。你可能需要用encoding/json（§4.5）包来分析输出的JSON格式的信息。
 package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"os/exec"
+)
+
+var (
+	RestMap map[string]int
+	depth   int
+)
+
+type Result struct {
+	Imports []string
+}
+
+func main() {
+	result := []string{}
+	RestMap = make(map[string]int)
+	TT("golang.org/x/net/html")
+	for k,_ := range RestMap{
+		result = append(result,k)
+	}
+	fmt.Println(result)
+}
+func TT(s string) {
+	re := new(Result)
+	cmd := exec.Command("/usr/local/bin/go", "list", "-e", "-json", s)
+	data, err := cmd.Output()
+	if err != nil {
+		fmt.Print(err)
+	}
+	json.Unmarshal(data, re)
+	for _, v := range re.Imports {
+		RestMap[v]++
+		depth++
+		TT(v)
+		if depth == 30 {
+			return
+		}
+
+	}
+
+}
+
