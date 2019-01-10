10a2ee2089b8e339a0b8ae30b13991d24371dbdd
googege <googege@icloud.com>
2019-01-10 20:36:20 +0800

    Update main.go

--- gop_homework/ch10/homework/test10.3/main.go
+++ gop_homework/ch10/homework/test10.3/main.go
@@ -1,3 +1,23 @@
 //练习 10.3: 从 http://gopl.io/ch1/helloworld?go-get=1 获取内容，
 // 查看本书的代码的真实托管的网址（go get请求HTML页面时包含了go-get参数，以区别普通的浏览器请求）。
 package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+func main(){
+	res,err := http.Get("http://gopl.io/ch1/helloworld?go-get=1")
+	if err != nil {
+		fmt.Println(err)
+	}
+	data,err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		fmt.Println(err)
+	}
+	fmt.Println(string(data))
+}
+
+
