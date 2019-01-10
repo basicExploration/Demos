ab29cc4aa43ec338101de7d3020331b20e944ab7
googege <googege@icloud.com>
2019-01-11 00:11:19 +0800

    add

--- gop_homework/ch10/homework/test10.4/main.go
+++ gop_homework/ch10/homework/test10.4/main.go
@@ -1,6 +1,6 @@
 //练习 10.4： 创建一个工具，根据命令行指定的参数，报告工作区所有依赖包指定的其它包集合。提示：
 // 你需要运行go list命令两次，一次用于初始化包，一次用于所有包。你可能需要用encoding/json（§4.5）包来分析输出的JSON格式的信息。
-package main
+package mo
 
 import (
 	"encoding/json"
@@ -22,14 +22,16 @@ type Result struct {
 }
 
 func init(){
-	flag.StringVar(&varS,"mo","github.com/googege","搜索所有的包")
+	flag.StringVar(&varS,"mo","github.com/googege","search all mo")
 	flag.Parse()
 }
-func main() {
+//show all modules.
+func Mo()[]string{
 	result := []string{}
 	RestMap = make(map[string]int)
 	TT(varS)
 	for   {// 此处一直阻塞，直到 start和end在一秒的时间区域内都是相等的时候然后退出。
+	// Blocked here until exit and end are equal in the time zone of one second and then exit.
 		start := len(RestMap)
 		time.Sleep(time.Second)
 		end := len(RestMap)
@@ -41,7 +43,7 @@ func main() {
 	for k, _ := range RestMap {
 		result = append(result, k)
 	}
-	fmt.Println(result)
+	return result
 
 }
 func TT(s string) {
@@ -55,10 +57,9 @@ func TT(s string) {
 	for _, v := range re.Imports {
 		sy.Lock()
 		RestMap[v]++
-		sy.Unlock()// 如果在lock前调用unlock那么会发生error错误
+		sy.Unlock()// 如果在lock前调用unlock那么会发生error错误If you call unlock before lock, an error will occur.
 		go TT(v)
 	}
 
 }
 
-//TODO 可以改成并发的。
