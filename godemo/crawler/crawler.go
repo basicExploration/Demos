5bbe9ae45a5c24bb74cc9805b85f6d7376e10816
googege <googege@icloud.com>
2019-01-11 21:14:23 +0800

    ad

--- godemo/crawler/crawler.go
+++ godemo/crawler/crawler.go
@@ -16,25 +16,30 @@ var (
 	sym  sync.Mutex
 	syw  sync.WaitGroup
 	syw1 sync.WaitGroup
-	m    = make(map[string]string)
 )
 
 // 设计理念 1 找到所有的img然后，获取所有的img的src，然后通过get将src创建到每个不同的文件里（使用jep包的decode和encode）
-func Crawler() map[string]string {
+func Crawler(){
+	t := make(chan struct{}, 20)
+	fmt.Println("正在开始执行")
 	syw.Add(n)
 	for i := 0; i < n; i++ {
 		go func(i int) {
+			t <- struct{}{}
 			defer func() {
 				if r := recover(); r != nil {
 					fmt.Println(r)
 				}
 			}()
 			s := strconv.FormatInt(int64(i), 10)
+			fmt.Println("开始获取页面")
 			res, err := http.Get(url + s)
+			<-t
 			if err != nil {
 				fmt.Println(err)
 			}
 			n, err := html.Parse(res.Body)
+			res.Body.Close()
 			if err != nil {
 				fmt.Println(err)
 			}
@@ -42,7 +47,6 @@ func Crawler() map[string]string {
 		}(i)
 	}
 	syw.Wait()
-	return m
 
 }
 
@@ -50,6 +54,7 @@ func dear(n *html.Node, dd string) {
 	if n.Type == html.ElementNode && n.Data == "img" {
 		for _, v := range n.Attr {
 			if v.Key == "src" {
+				fmt.Println("已经获取src将要创造图片")
 				go crearFile(dd, v.Val)
 			}
 
@@ -58,13 +63,32 @@ func dear(n *html.Node, dd string) {
 }
 
 func crearFile(k, v string) {
-	defer syw.Done()
+	t := make(chan struct{}, 20)
+	t <- struct{}{}
+	defer func() {
+		syw.Done()
+		fmt.Println(k, "已经完毕")
+	}()
+	fmt.Println("正在获取图片src")
 	res, err := http.Get(v)
+	<-t // 限制访问个数
 	defer res.Body.Close()
 	if err != nil {
 		fmt.Println(err)
 	}
-	im, _ := jpeg.Decode(res.Body)
-	file, _ := os.Create("./p3/" + k)
-	jpeg.Encode(file, im, &jpeg.Options{99})
+	im, err := jpeg.Decode(res.Body)
+	res.Body.Close()
+	if err != nil {
+		fmt.Println(err)
+	}
+	file, err := os.Create("./p3/" + k + ".jpg")
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = jpeg.Encode(file, im, &jpeg.Options{99})
+	if err != nil {
+		fmt.Println(err)
+	}
+	file.Close()
+	fmt.Println("图片制作完毕")
 }
