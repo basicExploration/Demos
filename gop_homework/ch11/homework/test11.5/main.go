0d0b116f475eb1b9dc97624635e2b9215ef252ac
googege <googege@icloud.com>
2019-01-11 20:15:09 +0800

    over

--- gop_homework/ch11/homework/test11.5/main.go
+++ gop_homework/ch11/homework/test11.5/main.go
@@ -1,2 +1,17 @@
 //练习11.5: 用表格驱动的技术扩展TestSplit测试，并打印期望的输出结果。
 package main
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestSplit(t *testing.T) {
+	s, sep := "a:b:c", ":"
+	words := strings.Split(s, sep)
+	if got, want := len(words), 3; got != want {
+		t.Errorf("Split(%q, %q) returned %d words, want %d",
+			s, sep, got, want)
+	}
+	// ...
+}
