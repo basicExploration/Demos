ebfa9cbde25cd04128c0a02ab31b02a81b260f03
googege <googege@icloud.com>
2019-01-12 21:05:29 +0800

    add

---
+++ godemo/crawler/p5/main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	"time"
+
+	"github.com/chromedp/cdproto/cdp"
+	"github.com/chromedp/cdproto/runtime"
+	"github.com/chromedp/chromedp"
+)
+
+func main() {
+	var err error
+
+	// create context
+	ctxt, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// create chrome instance
+	c, err := chromedp.New(ctxt, chromedp.WithLog(log.Printf))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// run task list
+	err = c.Run(ctxt, visible())
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// shutdown chrome
+	err = c.Shutdown(ctxt)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	// wait for chrome to finish
+	err = c.Wait()
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func visible() chromedp.Tasks {
+	var res *runtime.RemoteObject
+	return chromedp.Tasks{
+		chromedp.Navigate("https://m.930mh.com/manhua/14396/544283.html?p=35"),
+		chromedp.Evaluate(makeVisibleScript, &res),
+		chromedp.ActionFunc(func(context.Context, cdp.Executor) error {
+			log.Printf(">>> res: %+v", res)
+			return nil
+		}),
+		chromedp.WaitVisible(`#box1`),
+		chromedp.ActionFunc(func(context.Context, cdp.Executor) error {
+			log.Printf(">>>>>>>>>>>>>>>>>>>> BOX1 IS VISIBLE")
+			return nil
+		}),
+		chromedp.WaitVisible(`#box2`),
+		chromedp.ActionFunc(func(context.Context, cdp.Executor) error {
+			log.Printf(">>>>>>>>>>>>>>>>>>>> BOX2 IS VISIBLE")
+			return nil
+		}),
+		chromedp.ActionFunc(func(context.Context, cdp.Executor) error {
+			log.Printf(">>>>>>>>>>>>>>>>>>>> WAITING TO EXIT")
+			time.Sleep(150 * time.Second)
+			return errors.New("exiting")
+		}),
+	}
+}
+
+const (
+	makeVisibleScript = `setTimeout(function() {
+	document.querySelector('#box1').style.display = '';
+}, 30000);`
+)
