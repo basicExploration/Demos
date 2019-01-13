1cb882b4104ded8ed1deb6ddd09c231d54e895e4
googege <googege@icloud.com>
2019-01-13 15:43:52 +0800

    Update CRUD.sql

--- SQLTest/CRUD.sql
+++ SQLTest/CRUD.sql
@@ -6,3 +6,14 @@ INSERT INTO values (name,year) VALUES
 ("1",12),
 ("2",3),
 ("3",23); --  primary key 也就是主键不用写出啦了，它自己会增加，另外有默认值的也可以不写，但是没有默认值并且不能为null的一定要写出来。
+-- update
+UPDATE values SET name='1',year=1 WHERE id >10;
+--- update的计算
+UPDATE values SET name=name +1 WHERE id>10 AND id <5;
+-- ⚠️
+UPDATE values SET name="1";-- 像这种没有WHERE的语句，非常的危险，容易更新全部的信息。
+-- Delete
+DELETE FROM values WHERE id=12;
+-- ⚠️
+DELETE FROM values --这样就删掉了全部，很危险
+
