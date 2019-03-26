f291fa3fdeef1c8195194b49fa4d637d5aa992e8
3G <googege@icloud.com>
2019-03-26 13:56:03 +0800

    Update CRUD.sql

--- SQLTest/CRUD.sql
+++ SQLTest/CRUD.sql
@@ -1,13 +1,13 @@
 -- 增删改 insert delete update
 -- INSERT
-INSERT INTO values (name,year) VALUES ("12",1908); -- 往 values 表插入 name和year的数据，数据的实际值是 "12" 1908
+INSERT table_name SET name='fsdf',year=1; -- 往 values 表插入 name和year的数据，数据的实际值是 "12" 1908
 -- 多批次增加Insert
 INSERT INTO values (name,year) VALUES
 ("1",12),
 ("2",3),
 ("3",23); --  primary key 也就是主键不用写出啦了，它自己会增加，另外有默认值的也可以不写，但是没有默认值并且不能为null的一定要写出来。
 -- update
-UPDATE values SET name='1',year=1 WHERE id >10;
+UPDATE table_name SET name='1',year=1 WHERE id >10;
 --- update的计算
 UPDATE values SET name=name +1 WHERE id>10 AND id <5;
 -- ⚠️
