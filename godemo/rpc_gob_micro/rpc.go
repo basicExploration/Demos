8e0847ff39ff177c6984f68f2af47be095a98cff
googege <googege@icloud.com>
2019-05-15 10:37:15 +0800

    hello dart hello flutter

--- godemo/rpc_gob_micro/rpc.go
+++ godemo/rpc_gob_micro/rpc.go
@@ -1,1 +1,7 @@
 package main
+
+import "net/rpc"
+
+func tt(){
+	rpc.Client{}.Call()
+}
--- helloDart/hello.dart
+++ helloDart/hello.dart
@@ -1,23 +1,3 @@
-// hello dart.
-import 'dart:math';
-import 'package:angular/angular.dart';
-@Component(
-  selector: 'my-app',
-  template: '<h1>Hello {{name}}</h1>',
-)
-class AppComponent {
-  var name = 'Angular';
-}
-
-class Component {
-}
-void main(){
-  math.toString(1);
-   bool ddd = true;
-   print(ddd);
-  var  tt = {'a':'ff','b':56};
-  num a =12;
-  a = 5.0;
-  print(a);
-  
+main(){
+  print("hello dart hello flutter");
 }
