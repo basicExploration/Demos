55976bf9e1524ac7e637e31852614bbd86472194
googege <googege@icloud.com>
2019-05-23 00:11:12 +0800

    add

--- godemo/queue/1.go
+++ godemo/queue/1.go
@@ -1,3 +1,34 @@
 // 顺序队列
 
 package main
+
+import "fmt"
+
+func main(){
+	t := NewThing()
+	t.In("12")
+	t.In("13")
+	t.In("14")
+	t.In("15")
+	t.Out()
+	fmt.Println(t.value)
+}
+
+type thing struct {
+	value []interface{}
+}
+
+func NewThing()*thing{
+	return &thing{
+		make([]interface{},0),
+	}
+}
+
+
+func(t *thing)In(v interface{}){
+	t.value = append(t.value,v)
+}
+
+func(t *thing)Out(){
+	t.value = t.value[1:]
+}
