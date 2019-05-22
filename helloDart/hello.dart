9d71e3f6cef615cc35fee478463c8efecdc1cc93
googege <googege@icloud.com>
2019-05-22 17:39:40 +0800

    add

--- helloDart/hello.dart
+++ helloDart/hello.dart
@@ -1,3 +1,44 @@
-main(){
+import 'dart:ffi';
+  interface Dd6<T> {
+     T fff4();
+   };
+void main() async {
+  int a;
+  a = 12;
+  String b = "Flutter is the father of dart";
   print("hello dart hello flutter");
+  print("hello");
+  final String name = "bob";
+  var a1 = 3.134.toString();
+  var a333 =[1,2,3,4];
+  [1,2,3,4,5].forEach((ele)=> print(ele));
+   void aaad(args ) async{
+    
+  }
+  a333.forEach((k) => print('$k'));
+var a1111 = {
+  "12":"1221",
+} ;
+  var c = new Map.from(a1111);
+  print(c);
+dd();
+print(DD is! Object);
+DD()();
 }
+
+String hello(String from,String b,[int ddd]) => "$from::::$b:::[$ddd]";
+Void dd() {
+  print(hello("12","121"));
+}
+
+var aaff3a = [1,3,3,4,4];
+
+ Function DD() {
+
+  return () => print("${~122}");
+}
+Map<int,String> oo6 = {
+  1:"3",
+  34:"1221",
+};
+
---
+++ helloDart/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+)
+
+func main() {
+	if 12 > 1 {
+		fmt.Print("1")
+	}
+}
+
+func uit() {
+	fmt.Print("heiehieh")
+}
